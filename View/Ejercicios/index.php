857dce8d535c19dd5a35ed500bb87ee29ef598a3
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-07-31 16:04:58 -0400

    Creada página principal del módulo, mostrando opciones: Editor, Ejemplos y Ejercicios

---
+++ View/Ejercicios/index.php
@@ -0,0 +1,2 @@
+<h1>Ejercicios</h1>
+<p>En construcción.</p>
--- View/display.php
+++ View/display.php
@@ -1,7 +1,44 @@
 <h1>IDE: ambiente de desarrollo online</h1>
 <p>Este módulo provee funcionalidades básicas para poder compilar y ejecutar código en diferentes lenguajes de programación.</p>
 
-<ul>
-    <li><a href="<?=$_base?>/ide/editor">Editor</a></li>
-    <li><a href="<?=$_base?>/ide/ejemplos">Ejemplos</a></li>
-</ul>
+<style>
+#icons {
+    width: 620px;
+    margin: 0 auto;
+}
+#icons > div {
+    width: 150px;
+    float: left;
+    margin-right: 20px;
+    padding: 20px;
+    text-align: center;
+}
+#icons > div:last-child {
+    margin-right: 0;
+}
+#icons > div > a > span {
+    font-size: 18px;
+}
+</style>
+
+<div id="icons">
+    <div>
+        <a href="<?=$_base?>/ide/editor">
+            <img src="<?=$_base?>/ide/img/icons/128x128/editor.png" alt="" />
+            <span>Editor</span>
+        </a>
+    </div>
+    <div>
+        <a href="<?=$_base?>/ide/ejemplos">
+            <img src="<?=$_base?>/ide/img/icons/128x128/ejemplos.png" alt="" />
+            <span>Ejemplos</span>
+        </a>
+    </div>
+    <div>
+        <a href="<?=$_base?>/ide/ejercicios">
+            <img src="<?=$_base?>/ide/img/icons/128x128/ejercicios.png" alt="" />
+            <span>Ejercicios</span>
+        </a>
+    </div>
+</div>
+<div style="clear:both"></div>
