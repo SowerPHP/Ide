a3f3905bcf1a8b078cf6e52ca54c2f8bbba6a246
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-04-14 01:34:59 -0300

    De GPL a AGPL

--- Controller/Ejemplos.php
+++ Controller/Ejemplos.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * SowerPHP: Minimalist Framework for PHP
+ * SowerPHP
  * Copyright (C) SowerPHP (http://sowerphp.org)
  *
  * Este programa es software libre: usted puede redistribuirlo y/o
- * modificarlo bajo los términos de la Licencia Pública General GNU
+ * modificarlo bajo los términos de la Licencia Pública General Affero de GNU
  * publicada por la Fundación para el Software Libre, ya sea la versión
  * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
  * misma.
@@ -13,12 +13,12 @@
  * Este programa se distribuye con la esperanza de que sea útil, pero
  * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
  * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
- * Consulte los detalles de la Licencia Pública General GNU para obtener
- * una información más detallada.
+ * Consulte los detalles de la Licencia Pública General Affero de GNU para
+ * obtener una información más detallada.
  *
- * Debería haber recibido una copia de la Licencia Pública General GNU
+ * Debería haber recibido una copia de la Licencia Pública General Affero de GNU
  * junto a este programa.
- * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/agpl.html>.
  */
 
 namespace website\Ide;
@@ -40,11 +40,11 @@ class Controller_Ejemplos extends \Controller_App
     {
         $this->set('lenguajes', (new Model_Ejemplos())->getLenguages());
     }
-    
+
     /**
      * Método que busca en el modelo los ejemplos de un lenguaje específico y
      * los asigna para que la vista los pueda mostrar.
-     * @param 
+     * @param
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
      * @version 2014-07-22
      */
